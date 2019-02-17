55099a6c74b68f9bcac6e82808282b6f7c75aa53
Valentin LECLERE <vanakine@hotmail.fr>
2019-02-17 21:55:00 +0100

    Possibility to create one or multiple obstacles

--- js/main.js
+++ js/main.js
@@ -1,89 +1,136 @@
+var game;
+var scoreFont;
+var scoreValue;
+var scoreText;
+var player;
+var cursor;
+var hasTouchedGound;
+var bmd;
+var floor;
+var collider;
+var playerHealth;
+var colls, hamburger;
+var isHit;
+var waitImmort = 3;
+var wait = 0;
 var SimpleGame = /** @class */ (function () {
     function SimpleGame() {
-        this.waitImmort = 3;
-        this.wait = 0;
-        this.game = new Phaser.Game(800, 600, Phaser.AUTO, 'content', { preload: this.preload, create: this.create, update: this.update });
+        game = new Phaser.Game(800, 600, Phaser.AUTO, 'content', { preload: this.preload, create: this.create, update: this.update });
     }
     SimpleGame.prototype.preload = function () {
-        this.game.load.bitmapFont('gem', 'assets/font/gem.png', 'assets/font/gem.xml');
-        this.game.load.spritesheet('dude', 'assets/sprites/dude.png', 32, 48);
+        game.load.bitmapFont('gem', 'assets/font/gem.png', 'assets/font/gem.xml');
+        game.load.spritesheet('dude', 'assets/sprites/dude.png', 32, 48);
     };
     SimpleGame.prototype.create = function () {
         // INIT
-        this.hasTouchedGound = false;
-        this.playerHealth = 3;
-        this.isHit = false;
-        this.waitImmort = 3;
-        this.wait = 0;
-        this.game.physics.startSystem(Phaser.Physics.ARCADE);
-        this.collider = this.game.add.group();
-        this.collider.enableBody = true;
-        this.scoreText = "Score: ";
-        this.scoreValue = 0;
-        this.game.time.desiredFps = 30;
-        this.scoreFont = this.game.add.bitmapText(20, 20, 'gem', "", 34);
-        this.player = this.game.add.sprite(350, 400, 'dude');
-        this.player.anchor.x = 0.5;
-        this.player.anchor.y = 0.5;
-        this.game.physics.enable(this.player, Phaser.Physics.ARCADE);
-        this.player.body.bounce.y = 0;
-        this.player.body.collideWorldBounds = true;
-        this.player.body.setSize(20, 32, 5, 16);
-        this.player.animations.add('right', [5, 6, 7, 8], 10, true);
-        this.player.animations.play('right');
-        this.cursor = this.game.input.keyboard.createCursorKeys();
-        this.bmd = this.game.add.bitmapData(800, 50);
-        this.bmd.ctx.beginPath();
-        this.bmd.ctx.rect(0, 0, 800, 50);
-        this.bmd.ctx.fillStyle = '#ffff0f';
-        this.bmd.ctx.fill();
-        this.floor = this.game.add.sprite(0, 550, this.bmd);
-        this.game.physics.enable(this.floor, Phaser.Physics.ARCADE);
-        this.bmd.ctx.closePath();
-        this.floor.body.immovable = true;
-        this.floor.body.moves = false;
+        hasTouchedGound = false;
+        playerHealth = 3;
+        isHit = false;
+        waitImmort = 3;
+        wait = 0;
+        colls = [];
+        game.physics.setBoundsToWorld();
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+        collider = game.add.group();
+        collider.enableBody = true;
+        collider.physicsBodyType = Phaser.Physics.ARCADE;
+        scoreText = "Score: ";
+        scoreValue = 0;
+        game.time.desiredFps = 30;
+        scoreFont = game.add.bitmapText(20, 20, 'gem', "", 34);
+        player = game.add.sprite(350, 400, 'dude');
+        player.anchor.x = 0.5;
+        player.anchor.y = 0.5;
+        game.physics.enable(player, Phaser.Physics.ARCADE);
+        player.body.bounce.y = 0;
+        player.body.collideWorldBounds = true;
+        player.body.setSize(20, 32, 5, 16);
+        player.animations.add('right', [5, 6, 7, 8], 10, true);
+        player.animations.play('right');
+        cursor = game.input.keyboard.createCursorKeys();
+        bmd = game.add.bitmapData(800, 50);
+        bmd.ctx.beginPath();
+        bmd.ctx.rect(0, 0, 800, 50);
+        bmd.ctx.fillStyle = '#ffff0f';
+        bmd.ctx.fill();
+        floor = game.add.sprite(0, 550, bmd);
+        game.physics.enable(floor, Phaser.Physics.ARCADE);
+        bmd.ctx.closePath();
+        floor.body.immovable = true;
+        floor.body.moves = false;
+        // Collider creations
+        colls.push(createCollider(1000, 500));
+        colls.push(createCollider(1200, 400));
     };
     SimpleGame.prototype.update = function () {
-        if (!this.coll1) {
-            this.bmd = this.game.add.bitmapData(50, 50);
-            this.bmd.ctx.beginPath();
-            this.bmd.ctx.rect(0, 0, 50, 50);
-            this.bmd.ctx.fillStyle = '#ff0000';
-            this.bmd.ctx.fill();
-            this.coll1 = this.collider.create(800, 500, this.bmd);
-            this.bmd.ctx.closePath();
-            this.coll1.body.moves = false;
-            this.coll1.body.checkCollision.left = false;
+        if (!hamburger) {
+            bmd = game.add.bitmapData(50, 600);
+            bmd.ctx.beginPath();
+            bmd.ctx.rect(0, 0, 50, 600);
+            bmd.ctx.fillStyle = '#ff0000';
+            bmd.ctx.fill();
+            hamburger = collider.create(0, 0, bmd);
+            bmd.ctx.closePath();
+            hamburger.body.moves = false;
         }
-        else {
-            this.coll1.x -= 5;
-        }
+        var isDead = game.physics.arcade.collide(player, hamburger);
+        colliderManager(colls[0], isDead);
+        colliderManager(colls[1], isDead);
         //Collisions
-        var isGrounded = this.game.physics.arcade.collide(this.player, this.floor);
-        var hasTouched = this.game.physics.arcade.collide(this.player, this.collider);
-        if (hasTouched && this.wait < this.game.time.now / 1000) {
-            this.wait = this.game.time.now / 1000 + this.waitImmort;
-            this.player.x -= 100;
+        var isGrounded = game.physics.arcade.collide(player, floor);
+        var hasTouched = game.physics.arcade.collide(player, collider);
+        if (isDead) {
+            player.tint = 5;
+            player.animations.stop();
         }
-        if (!this.hasTouchedGound)
-            this.hasTouchedGound = true;
-        if (this.hasTouchedGound)
-            this.scoreValue += 30;
-        this.scoreFont.text = this.scoreText + Math.floor(this.scoreValue / 1000).toString();
-        this.game.debug.text("Time:" + this.game.time.now / 1000, 20, 100);
-        if (this.cursor.up.isDown && isGrounded && this.game.physics.arcade.gravity.y <= 500) {
-            this.player.body.velocity.y = -400;
+        if (hasTouched && wait < game.time.now / 1000 && !isDead) {
+            wait = game.time.now / 1000 + waitImmort;
+            player.x -= 100;
         }
-        if (this.cursor.down.isDown) {
-            this.player.body.velocity.y = this.player.body.velocity.y < 0 ? 0 : this.player.body.velocity.y;
-            this.game.physics.arcade.gravity.y = 2500;
+        if (!hasTouchedGound)
+            hasTouchedGound = true;
+        if (hasTouchedGound && !isDead)
+            scoreValue += 30;
+        scoreFont.text = scoreText + Math.floor(scoreValue / 1000).toString();
+        //this.game.debug.text("Time:" + this.game.time.now/1000, 20, 100);
+        if (cursor.up.isDown && isGrounded && game.physics.arcade.gravity.y <= 500) {
+            player.body.velocity.y = -400;
+        }
+        if (cursor.down.isDown) {
+            player.body.velocity.y = player.body.velocity.y < 0 ? 0 : player.body.velocity.y;
+            game.physics.arcade.gravity.y = 2500;
         }
         else {
-            this.game.physics.arcade.gravity.y = 500;
+            game.physics.arcade.gravity.y = 500;
         }
     };
     return SimpleGame;
 }());
+function createCollider(startx, starty) {
+    if (startx === void 0) { startx = 1000; }
+    if (starty === void 0) { starty = 500; }
+    var collid;
+    bmd = game.add.bitmapData(50, 50);
+    bmd.ctx.beginPath();
+    bmd.ctx.rect(0, 0, 50, 50);
+    bmd.ctx.fillStyle = '#ff0000';
+    bmd.ctx.fill();
+    collid = collider.create(startx, starty, bmd);
+    bmd.ctx.closePath();
+    collid.body.moves = false;
+    collid.body.checkCollision.left = false;
+    return collid;
+}
+function colliderManager(collid, isPlayerDead) {
+    if (!isPlayerDead) {
+        collid.x -= 5;
+        if (collid.x <= -50)
+            outOfBounds(collid);
+    }
+}
+function outOfBounds(collider) {
+    collider.x = 1000;
+}
 window.onload = function () {
     var game = new SimpleGame();
 };
--- src/main.ts
+++ src/main.ts
@@ -1,125 +1,168 @@
+var game: Phaser.Game;
+var scoreFont: Phaser.BitmapText;
+var scoreValue: number;
+var scoreText: string;
+var player: Phaser.Sprite;
+var cursor: Phaser.CursorKeys;
+var hasTouchedGound;
+var bmd: Phaser.BitmapData;
+var floor: Phaser.Sprite;
+var collider: Phaser.Group;
+var playerHealth: number;
+var colls: Array<Phaser.Sprite>, hamburger: Phaser.Sprite;
+var isHit: boolean;
+var waitImmort: number = 3;
+var wait: number = 0;
 
 class SimpleGame {
 
     constructor() {
-        this.game = new Phaser.Game(800, 600, Phaser.AUTO, 'content', { preload: this.preload, create: this.create, update: this.update });
+        game = new Phaser.Game(800, 600, Phaser.AUTO, 'content', { preload: this.preload, create: this.create, update: this.update });
     }
 
-    game: Phaser.Game;
-    scoreFont: Phaser.BitmapText;
-    scoreValue: number;
-    scoreText: string;
-    player: Phaser.Sprite;
-    cursor: Phaser.CursorKeys;
-    hasTouchedGound;
-    bmd: Phaser.BitmapData;
-    floor: Phaser.Sprite;
-    collider: Phaser.Group;
-    playerHealth: number;
-    coll1: Phaser.Sprite; coll2: Phaser.Sprite;
-    isHit: boolean;
-    waitImmort: number = 3;
-    wait: number = 0;
-
     preload() {
-        this.game.load.bitmapFont('gem', 'assets/font/gem.png', 'assets/font/gem.xml');
-        this.game.load.spritesheet('dude', 'assets/sprites/dude.png', 32, 48);
+        game.load.bitmapFont('gem', 'assets/font/gem.png', 'assets/font/gem.xml');
+        game.load.spritesheet('dude', 'assets/sprites/dude.png', 32, 48);
     }
 
     create() {
         // INIT
-        this.hasTouchedGound = false;
-        this.playerHealth = 3;
-        this.isHit = false;
-        this.waitImmort = 3;
-        this.wait = 0;
+        hasTouchedGound = false;
+        playerHealth = 3;
+        isHit = false;
+        waitImmort = 3;
+        wait = 0;
+        colls = [];
 
 
-        this.game.physics.startSystem(Phaser.Physics.ARCADE);
+        game.physics.setBoundsToWorld();
+        game.physics.startSystem(Phaser.Physics.ARCADE);
 
-        this.collider = this.game.add.group();
-        this.collider.enableBody = true
+        collider = game.add.group();
+        collider.enableBody = true;
+        collider.physicsBodyType = Phaser.Physics.ARCADE;
 
-        this.scoreText = "Score: ";
-        this.scoreValue = 0;
+        scoreText = "Score: ";
+        scoreValue = 0;
 
-        this.game.time.desiredFps = 30;
+        game.time.desiredFps = 30;
 
-        this.scoreFont = this.game.add.bitmapText(20, 20, 'gem', "", 34);
+        scoreFont = game.add.bitmapText(20, 20, 'gem', "", 34);
 
-        this.player = this.game.add.sprite(350, 400, 'dude');
-        this.player.anchor.x = 0.5;
-        this.player.anchor.y = 0.5;
-        this.game.physics.enable(this.player, Phaser.Physics.ARCADE);
+        player = game.add.sprite(350, 400, 'dude');
+        player.anchor.x = 0.5;
+        player.anchor.y = 0.5;
+        game.physics.enable(player, Phaser.Physics.ARCADE);
 
-        this.player.body.bounce.y = 0;
-        this.player.body.collideWorldBounds = true;
-        this.player.body.setSize(20, 32, 5, 16);
+        player.body.bounce.y = 0;
+        player.body.collideWorldBounds = true;
+        player.body.setSize(20, 32, 5, 16);
 
-        this.player.animations.add('right', [5, 6, 7, 8], 10, true);
-        this.player.animations.play('right');
+        player.animations.add('right', [5, 6, 7, 8], 10, true);
+        player.animations.play('right');
 
-        this.cursor = this.game.input.keyboard.createCursorKeys();
+        cursor = game.input.keyboard.createCursorKeys();
         
-        this.bmd = this.game.add.bitmapData(800, 50);
+        bmd = game.add.bitmapData(800, 50);
         
-        this.bmd.ctx.beginPath();
-        this.bmd.ctx.rect(0, 0, 800, 50);
-        this.bmd.ctx.fillStyle = '#ffff0f';
-        this.bmd.ctx.fill();
-        this.floor = this.game.add.sprite(0, 550, this.bmd);
-        this.game.physics.enable(this.floor, Phaser.Physics.ARCADE);
-        this.bmd.ctx.closePath();
+        bmd.ctx.beginPath();
+        bmd.ctx.rect(0, 0, 800, 50);
+        bmd.ctx.fillStyle = '#ffff0f';
+        bmd.ctx.fill();
+        floor = game.add.sprite(0, 550, bmd);
+        game.physics.enable(floor, Phaser.Physics.ARCADE);
+        bmd.ctx.closePath();
         
-        this.floor.body.immovable = true;
-        this.floor.body.moves = false;
+        floor.body.immovable = true;
+        floor.body.moves = false;
+
+        // Collider creations
+        colls.push(createCollider(1000, 500));
+        colls.push(createCollider(1200, 400));
     }
 
     update() {
-        if(!this.coll1) {
-            this.bmd = this.game.add.bitmapData(50, 50);
-            this.bmd.ctx.beginPath();
-            this.bmd.ctx.rect(0, 0, 50, 50);
-            this.bmd.ctx.fillStyle = '#ff0000';
-            this.bmd.ctx.fill();
-            this.coll1 = this.collider.create(800, 500, this.bmd);
-            this.bmd.ctx.closePath();
-            this.coll1.body.moves = false;
-            this.coll1.body.checkCollision.left = false;
-        } else {
-            this.coll1.x-=5;
+        if(!hamburger) {
+            bmd = game.add.bitmapData(50, 600);
+            bmd.ctx.beginPath();
+            bmd.ctx.rect(0, 0, 50, 600);
+            bmd.ctx.fillStyle = '#ff0000';
+            bmd.ctx.fill();
+            hamburger = collider.create(0, 0, bmd);
+            bmd.ctx.closePath();
+
+            hamburger.body.moves = false;
         }
+
+        var isDead = game.physics.arcade.collide(player, hamburger);
+
+        colliderManager(colls[0], isDead);
+        colliderManager(colls[1], isDead);
 
         //Collisions
-        var isGrounded = this.game.physics.arcade.collide(this.player, this.floor);
-        var hasTouched = this.game.physics.arcade.collide(this.player, this.collider);
+        var isGrounded = game.physics.arcade.collide(player, floor);
+        var hasTouched = game.physics.arcade.collide(player, collider);
 
-        if(hasTouched && this.wait < this.game.time.now/1000) {
-            this.wait = this.game.time.now/1000 + this.waitImmort;
-            this.player.x -= 100;
+        if(isDead) {
+            player.tint = 5;
+            player.animations.stop();
+        }
+
+        if(hasTouched && wait < game.time.now/1000 && !isDead) {
+            wait = game.time.now/1000 + waitImmort;
+            player.x -= 100;
         }
         
-        if(!this.hasTouchedGound)
-            this.hasTouchedGound = true
+        if(!hasTouchedGound)
+            hasTouchedGound = true
 
-        if (this.hasTouchedGound)
-            this.scoreValue += 30;
+        if (hasTouchedGound && !isDead)
+            scoreValue += 30;
 
-        this.scoreFont.text = this.scoreText + Math.floor(this.scoreValue / 1000).toString();
-        this.game.debug.text("Time:" + this.game.time.now/1000, 20, 100);
+        scoreFont.text = scoreText + Math.floor(scoreValue / 1000).toString();
+        //this.game.debug.text("Time:" + this.game.time.now/1000, 20, 100);
 
-        if (this.cursor.up.isDown && isGrounded && this.game.physics.arcade.gravity.y <= 500) {
-            this.player.body.velocity.y = -400;
+        if (cursor.up.isDown && isGrounded && game.physics.arcade.gravity.y <= 500) {
+            player.body.velocity.y = -400;
         }
 
-        if (this.cursor.down.isDown) {
-            this.player.body.velocity.y = this.player.body.velocity.y < 0 ? 0 : this.player.body.velocity.y;
-            this.game.physics.arcade.gravity.y = 2500;
+        if (cursor.down.isDown) {
+            player.body.velocity.y = player.body.velocity.y < 0 ? 0 : player.body.velocity.y;
+            game.physics.arcade.gravity.y = 2500;
         } else {
-            this.game.physics.arcade.gravity.y = 500;
+            game.physics.arcade.gravity.y = 500;
         }
     }
 
+}
+
+function createCollider(startx: number = 1000, starty: number = 500) {
+    let collid;
+
+    bmd = game.add.bitmapData(50, 50);
+    bmd.ctx.beginPath();
+    bmd.ctx.rect(0, 0, 50, 50);
+    bmd.ctx.fillStyle = '#ff0000';
+    bmd.ctx.fill();
+    collid = collider.create(startx, starty, bmd);
+    bmd.ctx.closePath();
+
+    collid.body.moves = false;
+    collid.body.checkCollision.left = false;
+        
+    return collid
+}
+
+function colliderManager(collid: Phaser.Sprite, isPlayerDead: boolean) {
+    if(!isPlayerDead) {
+        collid.x-=5;
+        if(collid.x <= -50)
+            outOfBounds(collid);
+    }
+}
+
+function outOfBounds(collider: Phaser.Sprite) {
+    collider.x = 1000;
 }
 
 window.onload = () => {
